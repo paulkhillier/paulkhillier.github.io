46063e55233c1e730ccf7a2c18884aa0150f4412
paulkhillier <paulkhillier@gmail.com>
2016-10-05 18:53:30 -0400

    Initial Version
    Test

---
+++ js/main.js
@@ -0,0 +1,16 @@
+<!-- Animated scroll to anchor -->
+
+$(function() {
+  $('a[href*="#"]:not([href="#"],[href="#myCarousel"],[href="#testimonials"],[href="#bs-example-navbar-collapse-1"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1200);
+        return false;
+      }
+    }
+  });
+});
