49a582a94cba75de03461d0109e6a543f4f10f5b
paulkhillier <paulkhillier@gmail.com>
2016-10-17 16:11:33 -0400

    form altert t2

--- js/main.js
+++ js/main.js
@@ -59,10 +59,11 @@ $("#contactForm").validate({
   }
 });
 
-/*When clicking on Full hide fail/success boxes */
+/*When clicking on Full hide fail/success boxes
 $('#name').focus(function() {
-    $('#success').html('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+    $('#success').html('');
 });
+*/
 
 /*
 <!-- Animsition Page Transitions -->
